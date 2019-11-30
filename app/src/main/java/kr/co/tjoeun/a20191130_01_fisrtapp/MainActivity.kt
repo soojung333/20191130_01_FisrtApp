3ae962e1f8430894689f10bbcf1724c9fae2e6b4
blessme333 <blessme333@gmail.com>
2019-11-30 12:18:05 +0900

    연습용 주석(메모) 작성

--- app/src/main/java/kr/co/tjoeun/a20191130_01_fisrtapp/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/a20191130_01_fisrtapp/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+
+//    연습용
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
